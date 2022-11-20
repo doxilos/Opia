e46fb61d3505510a93f59ba609ffced122077796
Nurbaki Vural <nurbakivural@gmail.com>
2022-11-20 14:47:58 +0300

    fixed details page

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react"
 import {
     Paper,
     Typography,
@@ -6,25 +6,35 @@ import {
     Card,
     CardMedia,
     CardContent,
-    Grid, Grow, Button, CardActions, CardActionArea, CircularProgress
-} from "@mui/material";
-import {useDispatch, useSelector} from "react-redux";
+    Grid,
+    Grow,
+    Button,
+    CardActions,
+    CardActionArea,
+    CircularProgress,
+    Box,
+} from "@mui/material"
+import { useDispatch, useSelector } from "react-redux"
 import moment from "moment"
-import {useParams, useNavigate, Link} from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom"
 
-import {deletePost, getPost, getPostsBySearch, likePost} from "../../actions/posts";
+import {
+    deletePost,
+    getPost,
+    getPostsBySearch,
+    likePost,
+} from "../../actions/posts"
 import useStyles from "./styles"
-import CommentSection from "./CommentSection";
-import {Delete, MoreHoriz} from "@mui/icons-material";
-import Grid2 from "@mui/material/Unstable_Grid2";
+import CommentSection from "./CommentSection"
+import { Delete, MoreHoriz } from "@mui/icons-material"
+import Grid2 from "@mui/material/Unstable_Grid2"
 
 const PostDetails = () => {
-
-    const {post, posts, isLoading} = useSelector((state) => state.posts)
+    const { post, posts, isLoading } = useSelector((state) => state.posts)
     const dispatch = useDispatch()
     const navigate = useNavigate()
     const classes = useStyles()
-    const {id} = useParams()
+    const { id } = useParams()
 
     const openPost = (_id) => {
         // navigate(`/posts/${_id}`)
@@ -33,14 +43,15 @@ const PostDetails = () => {
 
     useEffect(() => {
         dispatch(getPost(id))
-    }, [id]);
+    }, [id])
 
     useEffect(() => {
         if (post) {
-            dispatch(getPostsBySearch({search: "none", tags: post.tags.join(",")}))
+            dispatch(
+                getPostsBySearch({ search: "none", tags: post.tags.join(",") })
+            )
         }
-    }, [post]);
-
+    }, [post])
 
     if (!post) return null
 
@@ -48,106 +59,164 @@ const PostDetails = () => {
         return (
             <Grow in out="true">
                 <Paper elevation={6} className={classes.loadingPaper}>
-                    <CircularProgress/>
+                    <CircularProgress />
                 </Paper>
             </Grow>
         )
     }
 
-    const recommendedPosts = posts.filter(({_id}) => _id !== post._id)
+    const recommendedPosts = posts.filter(({ _id }) => _id !== post._id)
 
     return (
-
         <Grow in timeout={600}>
-
-            <Paper>
-                
-            </Paper>
-
-            <Paper style={{padding: '20px', borderRadius: '15px'}} elevation={6}>
-                <div className={classes.card}>
-                    <div className={classes.section}>
-                        <Typography variant="h3" component="h2">{post.title}</Typography>
-                        <Typography gutterBottom variant="h6" color="textSecondary"
-                                    component="h2">{post.tags.map((tag, index) => (
-                            <Link key={index} to={`/tags/${tag}`} style={{textDecoration: 'none', color: '#3f51b5'}}>
-                                {` #${tag} `}
-                            </Link>
-                        ))}
+            <Paper
+                style={{ padding: "20px", borderRadius: "15px" }}
+                elevation={6}
+            >
+                <Grid2 container spacing={2}>
+                    <Grid2 xs={12} sm={12} md={6} xl={6}>
+                        <Typography variant="h3" component="h2">
+                            {post.title}
                         </Typography>
-                        <Typography gutterBottom variant="body1" component="p">{post.message}</Typography>
+                        <Typography
+                            gutterBottom
+                            variant="h6"
+                            color="textSecondary"
+                            component="h2"
+                        >
+                            {post.tags.map((tag, index) => (
+                                <Link
+                                    key={index}
+                                    to={`/tags/${tag}`}
+                                    style={{
+                                        textDecoration: "none",
+                                        color: "#3f51b5",
+                                    }}
+                                >
+                                    {` #${tag} `}
+                                </Link>
+                            ))}
+                        </Typography>
+                        <Typography gutterBottom variant="body1" component="p">
+                            {post.message}
+                        </Typography>
                         <Typography variant="h6">
                             Created by:
-                            <Link to={`/creators/${post.name}`} style={{textDecoration: 'none', color: '#3f51b5'}}>
+                            <Link
+                                to={`/creators/${post.name}`}
+                                style={{
+                                    textDecoration: "none",
+                                    color: "#3f51b5",
+                                }}
+                            >
                                 {` ${post.name}`}
                             </Link>
                         </Typography>
-                        <Typography variant="body1">{moment(post.createdAt).fromNow()}</Typography>
-                        <Divider style={{margin: '20px 0'}}/>
-                        <Typography variant="body1"><strong>Realtime Chat - coming soon!</strong></Typography>
-                        <Divider style={{margin: '20px 0'}}/>
-                        <CommentSection post={post}/>
-                        <Divider style={{margin: '20px 0'}}/>
-                    </div>
-                        <img style={{width: "100%"}} src={post.selectedFile} alt={post.name}/>
-                </div>
+                        <Typography variant="body1">
+                            {moment(post.createdAt).fromNow()}
+                        </Typography>
+                        <Divider style={{ margin: "20px 0" }} />
+                        <Typography variant="body1">
+                            <strong>Realtime Chat - coming soon!</strong>
+                        </Typography>
+                        <Divider style={{ margin: "20px 0" }} />
+                        <CommentSection post={post} />
+                        <Divider style={{ margin: "20px 0" }} />
+                    </Grid2>
+                    <Grid2 xs={12} sm={12} md={6} xl={6}>
+                        <img
+                            style={{ width: "100%" }}
+                            src={post.selectedFile}
+                            alt={post.name}
+                        />
+                    </Grid2>
+                </Grid2>
 
-                {
-                    recommendedPosts.length > 0 ? (
-                        <Grow in out="true" timeout={500}>
-                            <div className={classes.section}>
-                                <Typography gutterBottom variant="h5">You might also like:</Typography>
-                                <Divider/>
-                                <Grid container spacing={3} sx={{paddingTop: "25px"}}>
-
-                                    {
-                                        recommendedPosts.map(({
-                                                                  title,
-                                                                  message,
-                                                                  name,
-                                                                  likes,
-                                                                  selectedFile,
-                                                                  _id
-                                                              }) => (
-
-                                            <Grid className={classes.hover} key={_id} item xs={12} sm={12} md={6} lg={3}>
-                                                <Grow in timeout={600}>
-                                                    <Card sx={{maxWidth: 345}}>
-                                                        <CardActionArea onClick={() => openPost(_id)}>
-                                                            <CardMedia
-                                                                component="img"
-                                                                height="140"
-                                                                image={selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'}
-                                                                alt={name}
-                                                            />
-                                                            <CardContent>
-                                                                <Typography gutterBottom variant="h5" component="div">
-                                                                    {title}
-                                                                </Typography>
-                                                                <Typography variant="body2" color="text.secondary">
-                                                                    {message}
-                                                                </Typography>
-                                                                <Typography gutterBottom variant="h6" component="div">
-                                                                    Likes: {likes.length}
-                                                                </Typography>
-                                                            </CardContent>
-                                                        </CardActionArea>
-                                                    </Card>
-                                                </Grow>
-                                            </Grid>
-                                        ))
-                                    }
-                                </Grid>
-                            </div>
-                        </Grow>
-                    ) : (
-                        <Grow in out="true">
-                            <Paper elevation={6} className={classes.loadingPaper}>
-                                <CircularProgress/>
-                            </Paper>
-                        </Grow>
-                    )
-                }
+                {recommendedPosts.length > 0 ? (
+                    <Grow in out="true" timeout={500}>
+                        <div className={classes.section}>
+                            <Typography gutterBottom variant="h5">
+                                You might also like:
+                            </Typography>
+                            <Divider />
+                            <Grid
+                                container
+                                spacing={3}
+                                sx={{ paddingTop: "25px" }}
+                            >
+                                {recommendedPosts.map(
+                                    ({
+                                        title,
+                                        message,
+                                        name,
+                                        likes,
+                                        selectedFile,
+                                        _id,
+                                    }) => (
+                                        <Grid
+                                            className={classes.hover}
+                                            key={_id}
+                                            item
+                                            xs={12}
+                                            sm={12}
+                                            md={6}
+                                            lg={3}
+                                        >
+                                            <Grow in timeout={600}>
+                                                <Card sx={{ maxWidth: 345 }}>
+                                                    <CardActionArea
+                                                        onClick={() =>
+                                                            openPost(_id)
+                                                        }
+                                                    >
+                                                        <CardMedia
+                                                            component="img"
+                                                            height="140"
+                                                            image={
+                                                                selectedFile ||
+                                                                "https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png"
+                                                            }
+                                                            alt={name}
+                                                        />
+                                                        <CardContent>
+                                                            <Typography
+                                                                gutterBottom
+                                                                variant="h5"
+                                                                component="div"
+                                                            >
+                                                                {title}
+                                                            </Typography>
+                                                            <Typography
+                                                                variant="body2"
+                                                                color="text.secondary"
+                                                            >
+                                                                {message}
+                                                            </Typography>
+                                                            <Typography
+                                                                gutterBottom
+                                                                variant="h6"
+                                                                component="div"
+                                                            >
+                                                                Likes:{" "}
+                                                                {likes.length}
+                                                            </Typography>
+                                                        </CardContent>
+                                                    </CardActionArea>
+                                                </Card>
+                                            </Grow>
+                                        </Grid>
+                                    )
+                                )}
+                            </Grid>
+                        </div>
+                    </Grow>
+                ) : (
+                    <Grow in out="true">
+                        <Paper elevation={6} className={classes.loadingPaper}>
+                            <CircularProgress />
+                        </Paper>
+                    </Grow>
+                )}
             </Paper>
         </Grow>
     )
